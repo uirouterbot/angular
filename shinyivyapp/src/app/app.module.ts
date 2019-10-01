f1226aac739f3362febf9c2c0a69d9908046ae51
Chris Thielen <cthielen@netflix.com>
2019-09-30 19:32:07 -0700

    chore(shinyivy): create 2 components, delete welcome page boilerplate

--- shinyivyapp/src/app/app.module.ts
+++ shinyivyapp/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Component1Component, Component2Component],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
